27a5b94192d0660b2cc39a6c5c99efd77a775052
Scott Cheng <me@scottcheng.com>
2018-02-14 01:10:17 -0500

    why don't we dance

---
+++ src/pages/why-dont-we-dance.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Collection from "../components/collection";
+
+const content = [
+  "DSC01441.jpg",
+
+  "DSC01854.jpg",
+  "DSC01858.jpg",
+  "DSC01960.jpg",
+  "DSC01962.jpg",
+  "DSC01967.jpg",
+  "DSC01985.jpg",
+  "DSC01988.jpg",
+
+  "DSC02359.jpg",
+  "DSC02271.jpg",
+  "DSC02328.jpg",
+  "DSC02353.jpg",
+  "DSC02313.jpg",
+  "DSC02043.jpg",
+  "DSC02281.jpg",
+  "DSC02288.jpg",
+  "DSC02305.jpg",
+  "DSC02290.jpg",
+  "DSC02500.jpg",
+  "DSC02597.jpg",
+
+  "DSC01437.jpg",
+  "DSC01434.jpg",
+  "DSC01438.jpg",
+];
+
+export default () => (
+  <Collection title="why don't we dance" content={content} />
+);

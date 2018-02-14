5fdcfda01ae6dd2d09574a98d03bc0afef812534
Scott Cheng <me@scottcheng.com>
2018-02-14 01:16:27 -0500

    hmm it's 2018 now

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,6 @@ import Section from "./section";
 
 export default () => (
   <Section>
-    © 2017 <a href="http://scottcheng.com/">Scott Cheng</a>
+    © 2018 <a href="http://scottcheng.com/">Scott Cheng</a>
   </Section>
 );
